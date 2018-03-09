3c5acf143a16c13d872a027a4dd579af1cb1a88f
sulivann <nguyen.sulivan@gmail.com>
2018-03-09 12:56:40 +0100

    initial commit

---
+++ components/DummyComponent/DummyComponent.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="dummy">DummyComponent</div>
+</template>
+
+<style lang="scss">
+  @import './DummyComponent.scss';
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="title">
+    gd-folio-2018
+    <dummy/>
+  </h1>
+</template>
+<script>
+import Dummy from '~/components/DummyComponent/DummyComponent.vue';
+
+export default {
+  components: {
+    Dummy
+  }
+};
+</script>
